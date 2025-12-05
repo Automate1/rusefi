901526ebcf6a2f03362577b7f3ccc3ec4fbea521
Automate1 <jr@flagg.us>
2025-12-04 22:53:53 -0500

    Update board_configuration.h

--- firmware/config/boards/f407-discovery/board_configuration.h
+++ firmware/config/boards/f407-discovery/board_configuration.h
@@ -1,8 +1,12 @@
 #pragma once
 
-class Dfr0971;
+#include "dfr0971.h"
 
+// Forward declaration of BitbangI2c
+class BitbangI2c;
+
+// Only declare DAC here so other files can reference it
 extern Dfr0971 dac1;
-// extern Dfr0971 dac2;
+extern BitbangI2c bbI2C;
 
 // void registerDACLuaFunctions(lua_State* L);
