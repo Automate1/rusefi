28a07892b50121180ee98832210398b642ac1e58
Automate1 <jr@flagg.us>
2025-12-05 09:12:34 -0500

    Update dfr0971.h

--- firmware/hw_layer/drivers/dfr0971/dfr0971.h
+++ firmware/hw_layer/drivers/dfr0971/dfr0971.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "bitbang_i2c.h"
+#include <stdint.h>
 
+/**
+ * Driver for DFRobot DFR0971 12-bit DAC
+ * Supports 2 channels per device
+ */
+class Dfr0971 {
+public:
+    Dfr0971(BitbangI2c *i2cBus, uint8_t i2cAddress);
+
+    /**
+     * Set output for a DAC channel
+     * @param channel 0..1
+     * @param value 0..4095
+     */
+    void setOutput(uint8_t channel, uint16_t value);
+
+private:
+    BitbangI2c *i2c;
+    uint8_t address;
+};
